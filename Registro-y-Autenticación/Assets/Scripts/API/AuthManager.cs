19a43e784f979f6ceb3cd472f1b16f1fcac1659b
LS1653 <estebanpuerta2006@gmail.com>
2026-08-30 15:51:16 -0500

    Pruebas-en-la-escena

--- Registro-y-Autenticación/Assets/Scripts/API/AuthManager.cs
+++ Registro-y-Autenticación/Assets/Scripts/API/AuthManager.cs
@@ -16,4 +16,65 @@ public class AuthManager : MonoBehaviour
 
     [Header("UI")]
     [SerializeField] private TMP_Text statusText;
+
+    public void RegisterButtonClick()
+    {
+      StartCoroutine(RegisterUser());
+    }
+
+
+    private IEnumerator RegisterUser()
+    {
+        AuthData authData = new AuthData();
+    
+        authData.username = usernameInput.text;
+        authData.password = passwordInput.text;
+    
+        string jsonData = JsonUtility.ToJson(authData);
+    
+        Debug.Log("Sending JSON data: " + jsonData);
+    
+        UnityWebRequest request =
+            UnityWebRequest.Post(
+                Url + "/api/usuarios",
+                jsonData,
+                "application/json"
+            );
+    
+        yield return request.SendWebRequest();
+    
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+            Debug.LogError(request.downloadHandler.text);
+    
+            SetStatus("No se pudo registrar el usuario.");
+        }
+        else
+        {
+            Debug.Log(request.downloadHandler.text);
+    
+            UserResponse userResponse =
+                JsonUtility.FromJson<UserResponse>(
+                    request.downloadHandler.text
+                );
+    
+            Debug.Log(
+                "User registered: " +
+                userResponse.usuario.username
+            );
+    
+            SetStatus("Usuario registrado correctamente.");
+        }
+    }
+
+
+    private void SetStatus(string message)
+    {
+        if (statusText != null)
+        {
+            statusText.text = message;
+        }
+    }
 }
+
